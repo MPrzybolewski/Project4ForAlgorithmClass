bb5da9a3323f53d18e33bed035a4ef2e64104fa7
Marek Przybolewski <mprzybolewski@gmail.com>
2018-01-12 10:13:27 +0100

    add basic project

---
+++ Polynomial/Polynomial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Polynomial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
